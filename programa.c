ec2b50d013da6fc985c76c53b1e6f61c905328a5
Leonardo Hipolito <leonardohipolito@outlook.com>
2021-06-03 12:51:30 -0300

    Aula 2

--- programa.c
+++ programa.c
@@ -5,7 +5,7 @@
 typedef struct
 {
     char nome[50];
-    int notas[4];
+    float notas[4];
     int ativo;
 } Aluno;
 
@@ -22,19 +22,59 @@ int main(int argc, char const *argv[])
 }
 void menu()
 {
-    system("clear"); //linux
-    //system("cls"); //windows
     int op;
     do
     {
+        system("clear"); //linux
+        //system("cls"); //windows
         printf("\n1 - Cadastrar aluno\n2 - Remover aluno");
         printf("\n3 - Alunos reprovados\n4 - Pesquisar aluno\n");
         printf("\n0 - Sair\n");
         scanf("%d", &op);
+        getchar();
+        switch (op)
+        {
+        case 1:
+            cadastrar();
+            break;
+        }
     } while (op != 0);
 }
 void cadastrar()
 {
+    system("clear"); //linux
+    //system("cls"); //windows
+    char nome[50];
+    float notas[4];
+    int op;
+    do
+    {
+        printf("\nNome: ");
+        fgets(nome, sizeof(nome), stdin);
+        printf("\n1 Bimestre:");
+        scanf("%f", notas[0]);
+        printf("\n2 Bimestre:");
+        scanf("%f", notas[1]);
+        printf("\n3 Bimestre:");
+        scanf("%f", notas[2]);
+        printf("\n4 Bimestre:");
+        scanf("%f", notas[3]);
+        for (int i = 0; i < MAX_ALUNOS; i++)
+        {
+            if (alunos[i].ativo == 0)
+            {
+                alunos[i].notas[0] = notas[0];
+                alunos[i].notas[1] = notas[1];
+                alunos[i].notas[2] = notas[2];
+                alunos[i].notas[3] = notas[3];
+                strcpy(alunos[i].nome, nome);
+                break;
+            }
+        }
+
+        printf("\n1 - Continuar\n0 - Sair");
+        scanf("%d", &op);
+    } while (op != 0);
 }
 void remover()
 {
